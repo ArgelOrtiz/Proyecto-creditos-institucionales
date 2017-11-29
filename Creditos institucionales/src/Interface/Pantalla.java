1b3c9b8497ce3ccd1b374a36b10cbc888bb4993b
Argel Ortiz <ortizarenasargel@gmail.com>
2017-11-28 21:34:56 -0600

    1.2

--- Creditos institucionales/src/Interface/Pantalla.java
+++ Creditos institucionales/src/Interface/Pantalla.java
@@ -25,11 +25,17 @@ public class Pantalla extends JFrame {
     String BOTONES [];
     String ETIQUETAS[];
     
+    
     public Pantalla(){
         
+        //estructurar todos los componentes necesarios para la interface
         construye();
         arma();
         lanza();
+        
+        //indicar la primera pantalla a mostrar
+        inicioVisible(false);
+        alumnoVisible(true);
         
     }
  
@@ -43,12 +49,11 @@ public class Pantalla extends JFrame {
         //inicializar paneles y componentes a utilizar
         
         panelPrincipal = new JPanel();
-        paneles = new JPanel[6];
+        paneles = new JPanel[14];
         botones = new JButton[12];
         BOTONES = new String[botones.length];
         camposT = new JTextField[15];
-        
-        etiquetas = new JLabel[16];
+        etiquetas = new JLabel[17];
         ETIQUETAS = new String[etiquetas.length];
 
        
@@ -57,7 +62,7 @@ public class Pantalla extends JFrame {
     
     private void arma(){
         //Añadir espesificaciones a los componentes asi como caracteristicas
-        panelPrincipal.setLayout(new GridLayout(6,2));
+        panelPrincipal.setLayout(new GridLayout(7,2));
         panelPrincipal.setBounds(0, 0, 600, 400);
         
        this.setLayout(null);
@@ -91,17 +96,65 @@ public class Pantalla extends JFrame {
         //instanciar campos de texto
         for (int i = 0; i < camposT.length; i++) {
             
-            camposT[i] = new JTextField();
+            camposT[i] = new JTextField(25);
             
         }
         
         //Agregar distintos componentes al panelPrincipal principal
                 
-                //campo (1,1) del layout
-                paneles[0].add(etiquetas[0]);
+                //campo (1,1) del layout                //campo (1,2) del layout
+                paneles[0].add(etiquetas[0]);  
+                paneles[0].add(etiquetas[16]);
+                
+             
          
-                //campo (2,1) del layout
-                paneles[1].add(botones[0]);
+                //campo (2,1) del layout                //campo (2,2) del layout
+                paneles[2].add(botones[0]);             paneles[3].add(camposT[0]);
+                paneles[2].add(etiquetas[1]);
+                
+                //campo (3,1) del layout                //campo (3,2) del layout
+                paneles[4].add(botones[1]);             paneles[5].add(camposT[1]);
+                paneles[4].add(etiquetas[2]);
+                
+                //campo (4,1) del layout                //campo (4,2) del layout
+                paneles[6].add(botones[2]);             paneles[7].add(camposT[2]);
+                paneles[6].add(etiquetas[3]);
+                
+                //campo (5,1) del layout                //campo (5,2) del layout
+                paneles[8].add(etiquetas[4]);           paneles[9].add(camposT[3]);
+                
+                //campo (6,1) del layout                //campo (6,2) del layout
+                                                                    
+                                                       
+                //campo (7,1) del layout                //campo (7,2) del layout
+                paneles[12].add(botones[4]);            paneles[13].add(botones[3]);
+                                                        paneles[13].add(botones[5]);
+                
+        
+    }
+    
+    private void inicioVisible(boolean x){
+        etiquetas[0].setVisible(x);
+        botones[0].setVisible(x);
+        botones[1].setVisible(x);
+        botones[2].setVisible(x);
+        botones[3].setVisible(x);
+    }
+    
+    private void alumnoVisible(boolean x){
+        etiquetas[1].setVisible(x);
+        camposT[0].setVisible(x);
+        etiquetas[2].setVisible(x);
+        camposT[1].setVisible(x);
+        etiquetas[3].setVisible(x);
+        camposT[2].setVisible(x);
+        etiquetas[4].setVisible(x);
+        camposT[3].setVisible(x);
+        botones[4].setVisible(x);
+        botones[5].setVisible(x);
+        etiquetas[16].setVisible(x);
+        
+        
     }
     
     private void armarNombres(){
@@ -120,8 +173,8 @@ public class Pantalla extends JFrame {
         BOTONES[11] = "Cancelar";
         
         ETIQUETAS[0] = "Inicio";
-        ETIQUETAS[1] = "Numero de Control";
-        ETIQUETAS[2] = "Nombre del Alumno";
+        ETIQUETAS[1] = "Nombre del Alumno";
+        ETIQUETAS[2] = "Numero de Control";
         ETIQUETAS[3] = "Semestre en Curso";
         ETIQUETAS[4] = "Nombre del Tutor";
         ETIQUETAS[5] = "Fecha de la reunion";
@@ -135,13 +188,9 @@ public class Pantalla extends JFrame {
         ETIQUETAS[13] = "Nombre de la Conferencia";
         ETIQUETAS[14] = "Fecha de la Conferencia";
         ETIQUETAS[15] = "Horas Asistidas";
-      
-       
-        
-        
+        ETIQUETAS[16] = "Registro de Nuevo Alumno";
         
     }
-    
     
     private void lanza(){
         //Especificar las opciones del Jframe
@@ -151,9 +200,7 @@ public class Pantalla extends JFrame {
         //abrebiatura utilizada EXIT_ON_CLOSE = 3
         this.setDefaultCloseOperation(3);
         this.setLocationRelativeTo(null);   
-        
-        
+            
     }
-    
     
 }
